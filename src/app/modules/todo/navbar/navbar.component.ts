6746fd0e0526158dbf1f80c5990a32dd07f94ddb
victor <vmataba0@gmail.com>
2023-05-29 23:27:14 +0300

    refactor: add some updates

--- src/app/modules/todo/navbar/navbar.component.ts
+++ src/app/modules/todo/navbar/navbar.component.ts
@@ -1,23 +1,26 @@
-import {Component, OnDestroy} from '@angular/core';
-import {Store} from "@ngrx/store";
-import {logout} from "../../../store/actions/auth.action";
-import {Router} from "@angular/router";
+import { Component, OnDestroy } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { logout } from '../../../store/actions/auth.action';
+import { Router } from '@angular/router';
 import { CACHED_APP_STATE_KEY } from 'src/app/store/reducers';
+import { Observable } from 'rxjs';
+import { getFirstNameLetter } from 'src/app/store/selectors/auth.selector';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
+  firstNameLetter$: Observable<string | undefined>;
 
   constructor(private store: Store, private router: Router) {
+    this.firstNameLetter$ = store.select(getFirstNameLetter);
   }
 
   logout() {
-    this.store.dispatch(logout())
+    this.store.dispatch(logout());
     //TODO: add this in effects
-    this.router.navigate(['/login']).then()
+    this.router.navigate(['/login']).then();
   }
-
 }
--- src/app/store/selectors/auth.selector.ts
+++ src/app/store/selectors/auth.selector.ts
@@ -9,8 +9,11 @@ export const getLoaded = createSelector(getAuthState, state => state.loaded)
 export const getUser = createSelector(getAuthState, state => state.user)
 
 export const getFirstName = createSelector(getUser, user => user?.first_name)
+
 export const getIsGuest = createSelector(getAuthState, state => state.isGuest)
 
 export const getError = createSelector(getAuthState, state => state.error)
 
 export const getErrorMessage = createSelector(getError, (error:any) => error?.message)
+
+export const getFirstNameLetter = createSelector(getFirstName,firstName => firstName?.charAt(0))
