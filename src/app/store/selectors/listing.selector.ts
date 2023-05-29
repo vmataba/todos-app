d69c1696118c52119886f473851f039dc9419d53
victor <vmataba0@gmail.com>
2023-05-29 09:36:16 +0300

    refactor: sort listsing by title

--- src/app/store/selectors/listing.selector.ts
+++ src/app/store/selectors/listing.selector.ts
@@ -11,4 +11,13 @@ export const getLoaded = createSelector(getListingState,state => state.loaded)
 
 export const getError = createSelector(getListingState,state => state.error)
 
-export const getListings = createSelector(getListingState,state => state.listings)
+export const getListingsOld = createSelector(getListingState,state => state.listings)
+
+export const getListings = createSelector(getListingState, state => [...state.listings].sort((a,b) => {
+    const firstTitle = a.title.toLowerCase()
+    const secondTitle = b.title.toLowerCase()
+    if (firstTitle < secondTitle){
+        return -1;
+    }
+    return 1;
+}))
