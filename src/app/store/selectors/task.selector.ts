0ced013e7c0e7124fd77f1098502598e0e9f1d33
victor <vmataba0@gmail.com>
2023-05-29 09:44:38 +0300

    refactor: add tasks sorting based on their names

--- src/app/store/selectors/task.selector.ts
+++ src/app/store/selectors/task.selector.ts
@@ -1,9 +1,23 @@
-import { createSelector } from "@ngrx/store";
-import { getTodosState } from "./feature.selector";
-import { Task } from "../models/task.model";
+import { createSelector } from '@ngrx/store';
+import { getTodosState } from './feature.selector';
+import { Task } from '../models/task.model';
 
-export const getTaskState = createSelector(getTodosState, state => state.tasks)
+export const getTaskState = createSelector(
+  getTodosState,
+  (state) => state.tasks
+);
 
-export function getTasks(listingId?: number){
-    return createSelector(getTaskState,state => state.tasks.filter((task:Task) => task.listingId == listingId))
+export function getTasks(listingId?: number) {
+  return createSelector(getTaskState, (state) =>
+    [...state.tasks.filter((task: Task) => task.listingId == listingId)].sort(
+      (a, b) => {
+        const firstDescription = a.description.toLowerCase()
+        const secondDescription = b.description.toLowerCase()
+        if (firstDescription < secondDescription){
+            return -1
+        }
+        return 1;
+      }
+    )
+  );
 }
